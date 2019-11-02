da21987480b517a689ecfc277634b21917c7ec5c
robertkunzelmann <55854190+robertkunzelmann@users.noreply.github.com>
2019-11-02 18:16:24 +0100

    Zustandsraummodell nach Zomotor hinzugefügt (deutlich kleineres Modell mit gleichem Ergebnis)

---
+++ Regler/Zustandsraum_Zomotor/initm.m
@@ -0,0 +1,11 @@
+%mit diesem Skript wird die optimale Reglerstruktur k1 und k2 berechnet
+%zur vereinfachung wird v konstant auf die maximale kurvengesch. von
+%2.63m/s bei 1m kurvenradius gesetzt
+%als Eingangsgröße dient die Kurvenkrümmung c0 die aus dem Kamerasystem
+%kommt. Evtl besser: kamera liefert soll Kurswinkel delta_psi -> c0 =
+%f(delta_psi)
+systemMatrix_A = [0,2.63;0,0];%maximale Kurvengeschwindigkeit von 2.63m/s
+ausgangsVektor_cT = [0,2];%wegen vorder und hinterache gilt: Kurswinkel ist 0.5 des vorderachseinschlags (ungewichtetes mittel aus vorder und hinterachse)
+eingangsVektor_b = [0;-2.63];%delta_psi' = psi' - c0*v (letzteres ist der eingang, ersterees wird zusätlich in simulink addiert)
+system = ss(systemMatrix_A, eingangsVektor_b, ausgangsVektor_cT, 0);
+K = lqr(system, [3,0;0,3], 2)%tbd was sind Q, N, R? es handelt sich um Gewichtungsmatrixen in Diagonalform Q:Gewichtung des Zustandsvektors, R: Gewichtung der Eingangsvariable, N:Gewichtung beider größen???
