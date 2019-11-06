0fb321ec210611ac3d6ca20641b4ccf37285925f
robertkunzelmann <55854190+robertkunzelmann@users.noreply.github.com>
2019-11-06 14:57:45 +0100

    weitere überarbeitung von lqr

--- Regler/Zustandsraum_Zomotor/initm.m
+++ Regler/Zustandsraum_Zomotor/initm.m
@@ -5,7 +5,13 @@
 %kommt. Evtl besser: kamera liefert soll Kurswinkel delta_psi -> c0 =
 %f(delta_psi)
 systemMatrix_A = [0,2.63;0,0];%maximale Kurvengeschwindigkeit von 2.63m/s
-ausgangsVektor_cT = [0,2/0.5];%wegen vorder und hinterache gilt: Kurswinkel ist 0.5 des vorderachseinschlags (ungewichtetes mittel aus vorder und hinterachse)
-eingangsVektor_b = [0;-2.63];%delta_psi' = psi' - c0*v (letzteres ist der eingang, ersterees wird zusätlich in simulink addiert)
-system = ss(systemMatrix_A, eingangsVektor_b, ausgangsVektor_cT, 0);
-K = lqr(system, [3*0.5,0;0,0.1], 1)%tbd was sind Q, N, R? es handelt sich um Gewichtungsmatrixen in Diagonalform Q:Gewichtung des Zustandsvektors, R: Gewichtung der Eingangsvariable, N:Gewichtung beider größen???
+ausgangsVektor_cT = [0,2];%wegen vorder und hinterache gilt: Kurswinkel ist 0.5 des vorderachseinschlags (ungewichtetes mittel aus vorder und hinterachse)
+eingangsVektor_b = [0;2.63];%delta_psi' = -+psi' +- c0*v (letzteres ist der eingang, ersterees wird zusätlich in simulink addiert)
+system_s = ss(systemMatrix_A, eingangsVektor_b, ausgangsVektor_cT, 0);
+K = lqr(system_s, [10,0;0,0.3], 0.1)%tbd was sind Q, N, R? es handelt sich um Gewichtungsmatrixen in Diagonalform Q:Gewichtung des Zustandsvektors, R: Gewichtung der Eingangsvariable, N:Gewichtung beider größen???
+reglerMatrix_Ar = systemMatrix_A-eingangsVektor_b*K;
+system_r = ss(reglerMatrix_Ar, eingangsVektor_b, ausgangsVektor_cT, 0);
+pole(system_r)
+pole(tf(system_r)*tf('s'))
+%rlocus(system_r);%ergibt die Ortskurve hier noch Sinn?
+sim('zustandsRegler.slx');
