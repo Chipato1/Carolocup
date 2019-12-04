94668195eea0a8cfaee0a58ac29082896dda76ec
robertkunzelmann <55854190+robertkunzelmann@users.noreply.github.com>
2019-12-05 00:24:18 +0100

    neue pole für den filter

--- Regler/Zustandsvariablenfilter/init.m
+++ Regler/Zustandsvariablenfilter/init.m
@@ -7,6 +7,15 @@ b = [1; 0; 0];
 cT = [1 0 0];
 A = [0 1 0; 0 0 1; -1 -8 -80];
 
-filter = ss(A,b,cT,0);
-pole(filter)
-bode(filter)
+% filter = ss(A,b,cT,0);
+% pole(filter)
+% bode(filter)
+
+
+Am = [0 1; -0.5 -10];
+bm = [1;0];
+cTm = [1 0];
+e1 = [1 0];
+e2 = [0 1];
+ filterm = ss(Am,bm,cTm,0);
+ pole(filterm)
