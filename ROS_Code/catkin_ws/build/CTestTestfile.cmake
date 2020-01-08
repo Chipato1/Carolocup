43c6ea6b21a0f8184cbdfebcdb3ca70579371e00
chipato <cwhk12a@web.de>
2020-01-08 22:56:14 +0100

    Catkin WS mit Paket für Zustandsautomat ur Verhaltenssteuerung hinzugefuegt

---
+++ ROS_Code/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chipato/Desktop/Test_git/Carolocup/ROS_Code/catkin_ws/src
+# Build directory: /home/chipato/Desktop/Test_git/Carolocup/ROS_Code/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("state_machine")
---
+++ ROS_Code/catkin_ws/build/state_machine/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chipato/Desktop/Test_git/Carolocup/ROS_Code/catkin_ws/src/state_machine
+# Build directory: /home/chipato/Desktop/Test_git/Carolocup/ROS_Code/catkin_ws/build/state_machine
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
