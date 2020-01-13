c4e8813bbd812675e067ed7f608660447c63d0de
A. Stollburges <56261614+Stolli1998@users.noreply.github.com>
2020-01-13 20:10:42 +0100

    Stand 13.01.2020
    neuer Ordner

---
+++ new_all/new_all.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+#include "Lights.h"
+#include "Motor.h"
+#include "Servo.h"
+#include "USB_Handle.h"
+#define referenzvolatage 0.0021259765
+
+void setup() 
+{
+  lights_init();
+  servo_init();
+  motor_init();
+  usb_init();
+}
+
+void loop() 
+{
+  analogvalue_1 = analogRead(xxxx);//Einlesen des Pins vom Tiefpass
+  voltage_1 = referenzvolatage * analogvalue_1; // 5V/1024 = 0.0048V pro Schritt
+  if(voltage_1 <0.3)//Autonom betrieb
+  {
+      digitalWrite(flasher_left,Off); //blaue LED auschalten
+     digitalWrite(xxxxx,On);//Multiplexer Select Pin 
+     aktorik_knoten.spinOnce();
+  }
+  else//RC
+  {
+      digitalWrite(flasher_left,ON); //blaue LED anschalten
+      digitalWrite(xxxxx,Off);//Multiplexer Select Pin 
+      
+  }
+  lichter_knoten.spinOnce();
+}
