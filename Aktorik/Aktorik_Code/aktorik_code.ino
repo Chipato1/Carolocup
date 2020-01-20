a01900fcba367d84bb3aac4d98ac42adad781406
chipato <cwhk12a@web.de>
2020-01-20 21:16:36 +0100

    patch

--- Aktorik/Aktorik_Code/aktorik_code.ino
+++ Aktorik/Aktorik_Code/aktorik_code.ino
@@ -15,19 +15,19 @@ void setup()
 
 void loop() 
 {
-  analogvalue_1 = analogRead(xxxx);//Einlesen des Pins vom Tiefpass
+  //analogvalue_1 = analogRead(xxxx);//Einlesen des Pins vom Tiefpass
   voltage_1 = referenzvolatage * analogvalue_1; // 5V/1024 = 0.0048V pro Schritt
   if (voltage_1 < 0.3)//Autonom betrieb
   {
      digitalWrite(blue_light,Off); //blaue LED auschalten
-     digitalWrite(xxxxx,On);//Multiplexer Select Pin
+     //digitalWrite(xxxxx,On);//Multiplexer Select Pin
      aktorik_knoten.spinOnce();
      
   }
   else  //RC
   {
       digitalWrite(blue_light,ON); //blaue LED anschalten
-      digitalWrite(xxxxx,Off);//Multiplexer Select Pin 
+      //digitalWrite(xxxxx,Off);//Multiplexer Select Pin 
       motor_bewegung_RC_mode();
       
   }
