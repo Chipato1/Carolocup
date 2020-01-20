85bec974b908eea85777d5a834910103001e3402
robertkunzelmann <robert.kunzelmann@web.de>
2020-01-20 02:22:34 +0100

    ros erstellt

--- Regler/old_shit/Material_fuer_PI/Motorkennlinie/Untitled.m
+++ Regler/old_shit/Material_fuer_PI/Motorkennlinie/Untitled.m
@@ -1,8 +1,13 @@
 test1 = timeseries()
-test1.Time = DATA001.Time
-test1.Data = DATA001.AccX
-sim = size(DATA001.Time,1)
-sim Kalman_gefiltert.slx
-simout.Time(2:end) = DATA001.Time(1:end)
-figure(1)
-plot(simout.Time, simout.Data(:,2))
+test1.Time = 1:size(DATA008.Time,1)
+test1.Data = DATA008.AccX
+sim = size(DATA008.Time,1)
+sim2 = round(DATA008.Time(end))
+backup = DATA008.Time;
+
+% sim Kalman_gefiltert.slx
+% simout.Time(3:end) = DATA008.Time(1:end)
+% figure(1)
+% plot(simout.Time, simout.Data(:,2))
+% figure(2)
+% plot(simout.Time, simout.Data(:,4))
