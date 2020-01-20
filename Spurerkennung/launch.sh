7653122eee3cbfdff73fd30a0e3ad5264d34f45b
mrosnauer <mrosnauer@gmail.com>
2020-01-20 18:37:09 +0100

    Fix

--- Spurerkennung/launch.sh
+++ Spurerkennung/launch.sh
@@ -1,4 +1,4 @@
-roslaunch ueye_cam/launch/mono8.launch
+roslaunch src/ueye_cam/launch/mono8.launch
 rosrun image_proc rectify
 rosrun vision vision_lanedetectionnode
 rosrun image_view image_view image:=camera/image_raw
