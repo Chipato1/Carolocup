79da3a298c553319d8039def7fdf693aa7e5fe0c
robertkunzelmann <robert.kunzelmann@web.de>
2020-01-27 21:06:23 +0100

    filter überprüft

--- Regler/old_shit/Material_fuer_PI/Motorkennlinie/Untitled.m
+++ Regler/old_shit/Material_fuer_PI/Motorkennlinie/Untitled.m
@@ -4,7 +4,7 @@ test1.Data = DATA011.AccX
 sim = 130%size(DATA011.Time,1)
 sim2 = round(DATA011.Time(end))
 backup = DATA011.Time;
-
+[num, denum] = butter(4,0.1);
 % sim Kalman_gefiltert.slx
 % simout.Time(3:end) = DATA008.Time(1:end)
 % figure(1)
