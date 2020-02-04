07c3c66ce62f21377ba2cc8a11165890f5845da7
Ben-Micha <benne4.bp@gmail.com>
2020-02-04 21:46:16 +0100

    ROS Modell Steuerung eingefügt in Filter

---
+++ Regler/Filter/PI_Laengsregler_ert_rtw/PI_Laengsregler_data.cpp
@@ -0,0 +1,80 @@
+/*
+ * PI_Laengsregler_data.cpp
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "PI_Laengsregler".
+ *
+ * Model version              : 1.30
+ * Simulink Coder version : 9.2 (R2019b) 18-Jul-2019
+ * C++ source code generated on : Tue Feb  4 21:14:39 2020
+ *
+ * Target selection: ert.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Linux 64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "PI_Laengsregler.h"
+#include "PI_Laengsregler_private.h"
+
+/* Block parameters (default storage) */
+P_PI_Laengsregler_T PI_Laengsregler_P = {
+  /* Computed Parameter: Constant_Value
+   * Referenced by: '<S3>/Constant'
+   */
+  {
+    0.0F                               /* Data */
+  },
+
+  /* Computed Parameter: Constant_Value_l
+   * Referenced by: '<S4>/Constant'
+   */
+  {
+    0.0F                               /* Data */
+  },
+
+  /* Computed Parameter: Constant_Value_j
+   * Referenced by: '<S1>/Constant'
+   */
+  {
+    0                                  /* Data */
+  },
+
+  /* Computed Parameter: Radumfang_Gain
+   * Referenced by: '<S5>/Radumfang'
+   */
+  5.6851778F,
+
+  /* Computed Parameter: Getriebe_Gain
+   * Referenced by: '<S5>/Getriebe'
+   */
+  7.0F,
+
+  /* Start of '<S4>/Enabled Subsystem' */
+  {
+    /* Computed Parameter: Out1_Y0
+     * Referenced by: '<S7>/Out1'
+     */
+    {
+      0.0F                             /* Data */
+    }
+  }
+  ,
+
+  /* End of '<S4>/Enabled Subsystem' */
+
+  /* Start of '<S3>/Enabled Subsystem' */
+  {
+    /* Computed Parameter: Out1_Y0
+     * Referenced by: '<S6>/Out1'
+     */
+    {
+      0.0F                             /* Data */
+    }
+  }
+  /* End of '<S3>/Enabled Subsystem' */
+};
