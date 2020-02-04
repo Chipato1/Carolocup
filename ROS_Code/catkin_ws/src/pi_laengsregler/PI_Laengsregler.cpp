7cc47af68f2c703e161b59c226311f3ace4a4b81
Ben-Micha <benne4.bp@gmail.com>
2020-02-04 22:50:33 +0100

    Regler Nodes

---
+++ ROS_Code/catkin_ws/src/pi_laengsregler/PI_Laengsregler.cpp
@@ -0,0 +1,258 @@
+/*
+ * PI_Laengsregler.cpp
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "PI_Laengsregler".
+ *
+ * Model version              : 1.30
+ * Simulink Coder version : 9.2 (R2019b) 18-Jul-2019
+ * C++ source code generated on : Tue Feb  4 21:14:39 2020
+ *
+ * Target selection: ert.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Linux 64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "PI_Laengsregler.h"
+#include "PI_Laengsregler_private.h"
+
+/* Block signals (default storage) */
+B_PI_Laengsregler_T PI_Laengsregler_B;
+
+/* Block states (default storage) */
+DW_PI_Laengsregler_T PI_Laengsregler_DW;
+
+/* Real-time model */
+RT_MODEL_PI_Laengsregler_T PI_Laengsregler_M_ = RT_MODEL_PI_Laengsregler_T();
+RT_MODEL_PI_Laengsregler_T *const PI_Laengsregler_M = &PI_Laengsregler_M_;
+
+/* Forward declaration for local functions */
+static void matlabCodegenHandle_matlabCod_l(ros_slros_internal_block_Subs_T *obj);
+static void matlabCodegenHandle_matlabCodeg(ros_slros_internal_block_Publ_T *obj);
+
+/*
+ * System initialize for enable system:
+ *    '<S3>/Enabled Subsystem'
+ *    '<S4>/Enabled Subsystem'
+ */
+void PI_Laengs_EnabledSubsystem_Init(B_EnabledSubsystem_PI_Laengsr_T *localB,
+  P_EnabledSubsystem_PI_Laengsr_T *localP)
+{
+  /* SystemInitialize for Outport: '<S6>/Out1' */
+  localB->In1 = localP->Out1_Y0;
+}
+
+/*
+ * Output and update for enable system:
+ *    '<S3>/Enabled Subsystem'
+ *    '<S4>/Enabled Subsystem'
+ */
+void PI_Laengsregle_EnabledSubsystem(boolean_T rtu_Enable, const
+  SL_Bus_PI_Laengsregler_std_msgs_Float32 *rtu_In1,
+  B_EnabledSubsystem_PI_Laengsr_T *localB)
+{
+  /* Outputs for Enabled SubSystem: '<S3>/Enabled Subsystem' incorporates:
+   *  EnablePort: '<S6>/Enable'
+   */
+  if (rtu_Enable) {
+    /* Inport: '<S6>/In1' */
+    localB->In1 = *rtu_In1;
+  }
+
+  /* End of Outputs for SubSystem: '<S3>/Enabled Subsystem' */
+}
+
+static void matlabCodegenHandle_matlabCod_l(ros_slros_internal_block_Subs_T *obj)
+{
+  if (!obj->matlabCodegenIsDeleted) {
+    obj->matlabCodegenIsDeleted = true;
+  }
+}
+
+static void matlabCodegenHandle_matlabCodeg(ros_slros_internal_block_Publ_T *obj)
+{
+  if (!obj->matlabCodegenIsDeleted) {
+    obj->matlabCodegenIsDeleted = true;
+  }
+}
+
+/* Model step function */
+void PI_Laengsregler_step(void)
+{
+  SL_Bus_PI_Laengsregler_std_msgs_Float32 b_varargout_2;
+  boolean_T b_varargout_1;
+  SL_Bus_PI_Laengsregler_std_msgs_Int16 rtb_BusAssignment;
+  real32_T tmp;
+
+  /* Outputs for Atomic SubSystem: '<Root>/Subscribe' */
+  /* MATLABSystem: '<S3>/SourceBlock' */
+  b_varargout_1 = Sub_PI_Laengsregler_80.getLatestMessage(&b_varargout_2);
+
+  /* Outputs for Enabled SubSystem: '<S3>/Enabled Subsystem' */
+  PI_Laengsregle_EnabledSubsystem(b_varargout_1, &b_varargout_2,
+    &PI_Laengsregler_B.EnabledSubsystem);
+
+  /* End of Outputs for SubSystem: '<S3>/Enabled Subsystem' */
+  /* End of Outputs for SubSystem: '<Root>/Subscribe' */
+
+  /* DataTypeConversion: '<Root>/Data Type Conversion' incorporates:
+   *  Gain: '<S5>/Getriebe'
+   *  Gain: '<S5>/Radumfang'
+   */
+  tmp = static_cast<real32_T>(floor(static_cast<real_T>
+    ((PI_Laengsregler_P.Radumfang_Gain *
+      PI_Laengsregler_B.EnabledSubsystem.In1.Data *
+      PI_Laengsregler_P.Getriebe_Gain))));
+  if (rtIsNaNF(tmp) || rtIsInfF(tmp)) {
+    tmp = 0.0F;
+  } else {
+    tmp = static_cast<real32_T>(fmod(static_cast<real_T>(tmp), 65536.0));
+  }
+
+  /* BusAssignment: '<Root>/Bus Assignment' incorporates:
+   *  DataTypeConversion: '<Root>/Data Type Conversion'
+   */
+  rtb_BusAssignment.Data = static_cast<int16_T>((tmp < 0.0F ?
+    static_cast<int32_T>(static_cast<int16_T>(-static_cast<int16_T>
+    (static_cast<uint16_T>(-tmp)))) : static_cast<int32_T>(static_cast<int16_T>(
+    static_cast<uint16_T>(tmp)))));
+
+  /* Outputs for Atomic SubSystem: '<Root>/Publish' */
+  /* MATLABSystem: '<S2>/SinkBlock' */
+  Pub_PI_Laengsregler_79.publish(&rtb_BusAssignment);
+
+  /* End of Outputs for SubSystem: '<Root>/Publish' */
+
+  /* Outputs for Atomic SubSystem: '<Root>/Subscribe1' */
+  /* MATLABSystem: '<S4>/SourceBlock' */
+  b_varargout_1 = Sub_PI_Laengsregler_128.getLatestMessage(&b_varargout_2);
+
+  /* Outputs for Enabled SubSystem: '<S4>/Enabled Subsystem' */
+  PI_Laengsregle_EnabledSubsystem(b_varargout_1, &b_varargout_2,
+    &PI_Laengsregler_B.EnabledSubsystem_n);
+
+  /* End of Outputs for SubSystem: '<S4>/Enabled Subsystem' */
+  /* End of Outputs for SubSystem: '<Root>/Subscribe1' */
+}
+
+/* Model initialize function */
+void PI_Laengsregler_initialize(void)
+{
+  /* Registration code */
+
+  /* initialize non-finites */
+  rt_InitInfAndNaN(sizeof(real_T));
+
+  /* block I/O */
+  (void) memset((static_cast<void *>(&PI_Laengsregler_B)), 0,
+                sizeof(B_PI_Laengsregler_T));
+
+  /* states (dwork) */
+  (void) memset(static_cast<void *>(&PI_Laengsregler_DW), 0,
+                sizeof(DW_PI_Laengsregler_T));
+
+  {
+    char_T tmp[14];
+    char_T tmp_0[17];
+    int32_T i;
+    static const char_T tmp_1[16] = { '/', 't', 'r', 'j', '_', 't', 'a', 'r',
+      'g', 'e', 't', 'S', 'p', 'e', 'e', 'd' };
+
+    static const char_T tmp_2[13] = { '/', 'c', 't', 'l', '_', 'm', 'o', 't',
+      'o', 'r', 'R', 'p', 'm' };
+
+    static const char_T tmp_3[13] = { '/', 'c', 't', 'l', '_', 'v', 'e', 'l',
+      'o', 'c', 'i', 't', 'y' };
+
+    /* Start for Atomic SubSystem: '<Root>/Subscribe' */
+    /* Start for MATLABSystem: '<S3>/SourceBlock' */
+    PI_Laengsregler_DW.obj_j.matlabCodegenIsDeleted = false;
+    PI_Laengsregler_DW.objisempty_c = true;
+    PI_Laengsregler_DW.obj_j.isInitialized = 1;
+    for (i = 0; i < 16; i++) {
+      tmp_0[i] = tmp_1[i];
+    }
+
+    tmp_0[16] = '\x00';
+    Sub_PI_Laengsregler_80.createSubscriber(tmp_0, 1);
+    PI_Laengsregler_DW.obj_j.isSetupComplete = true;
+
+    /* End of Start for MATLABSystem: '<S3>/SourceBlock' */
+    /* End of Start for SubSystem: '<Root>/Subscribe' */
+
+    /* Start for Atomic SubSystem: '<Root>/Publish' */
+    /* Start for MATLABSystem: '<S2>/SinkBlock' */
+    PI_Laengsregler_DW.obj.matlabCodegenIsDeleted = false;
+    PI_Laengsregler_DW.objisempty_f = true;
+    PI_Laengsregler_DW.obj.isInitialized = 1;
+    for (i = 0; i < 13; i++) {
+      tmp[i] = tmp_2[i];
+    }
+
+    tmp[13] = '\x00';
+    Pub_PI_Laengsregler_79.createPublisher(tmp, 1);
+    PI_Laengsregler_DW.obj.isSetupComplete = true;
+
+    /* End of Start for MATLABSystem: '<S2>/SinkBlock' */
+    /* End of Start for SubSystem: '<Root>/Publish' */
+
+    /* Start for Atomic SubSystem: '<Root>/Subscribe1' */
+    /* Start for MATLABSystem: '<S4>/SourceBlock' */
+    PI_Laengsregler_DW.obj_o.matlabCodegenIsDeleted = false;
+    PI_Laengsregler_DW.objisempty = true;
+    PI_Laengsregler_DW.obj_o.isInitialized = 1;
+    for (i = 0; i < 13; i++) {
+      tmp[i] = tmp_3[i];
+    }
+
+    tmp[13] = '\x00';
+    Sub_PI_Laengsregler_128.createSubscriber(tmp, 1);
+    PI_Laengsregler_DW.obj_o.isSetupComplete = true;
+
+    /* End of Start for MATLABSystem: '<S4>/SourceBlock' */
+    /* End of Start for SubSystem: '<Root>/Subscribe1' */
+  }
+
+  /* SystemInitialize for Atomic SubSystem: '<Root>/Subscribe' */
+  /* SystemInitialize for Enabled SubSystem: '<S3>/Enabled Subsystem' */
+  PI_Laengs_EnabledSubsystem_Init(&PI_Laengsregler_B.EnabledSubsystem,
+    &PI_Laengsregler_P.EnabledSubsystem);
+
+  /* End of SystemInitialize for SubSystem: '<S3>/Enabled Subsystem' */
+  /* End of SystemInitialize for SubSystem: '<Root>/Subscribe' */
+
+  /* SystemInitialize for Atomic SubSystem: '<Root>/Subscribe1' */
+  /* SystemInitialize for Enabled SubSystem: '<S4>/Enabled Subsystem' */
+  PI_Laengs_EnabledSubsystem_Init(&PI_Laengsregler_B.EnabledSubsystem_n,
+    &PI_Laengsregler_P.EnabledSubsystem_n);
+
+  /* End of SystemInitialize for SubSystem: '<S4>/Enabled Subsystem' */
+  /* End of SystemInitialize for SubSystem: '<Root>/Subscribe1' */
+}
+
+/* Model terminate function */
+void PI_Laengsregler_terminate(void)
+{
+  /* Terminate for Atomic SubSystem: '<Root>/Subscribe' */
+  /* Terminate for MATLABSystem: '<S3>/SourceBlock' */
+  matlabCodegenHandle_matlabCod_l(&PI_Laengsregler_DW.obj_j);
+
+  /* End of Terminate for SubSystem: '<Root>/Subscribe' */
+
+  /* Terminate for Atomic SubSystem: '<Root>/Publish' */
+  /* Terminate for MATLABSystem: '<S2>/SinkBlock' */
+  matlabCodegenHandle_matlabCodeg(&PI_Laengsregler_DW.obj);
+
+  /* End of Terminate for SubSystem: '<Root>/Publish' */
+
+  /* Terminate for Atomic SubSystem: '<Root>/Subscribe1' */
+  /* Terminate for MATLABSystem: '<S4>/SourceBlock' */
+  matlabCodegenHandle_matlabCod_l(&PI_Laengsregler_DW.obj_o);
+
+  /* End of Terminate for SubSystem: '<Root>/Subscribe1' */
+}
---
+++ ROS_Code/catkin_ws/src/pi_laengsregler/PI_Laengsregler_private.h
@@ -0,0 +1,33 @@
+/*
+ * PI_Laengsregler_private.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "PI_Laengsregler".
+ *
+ * Model version              : 1.30
+ * Simulink Coder version : 9.2 (R2019b) 18-Jul-2019
+ * C++ source code generated on : Tue Feb  4 21:14:39 2020
+ *
+ * Target selection: ert.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Linux 64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_PI_Laengsregler_private_h_
+#define RTW_HEADER_PI_Laengsregler_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "PI_Laengsregler.h"
+
+extern void PI_Laengs_EnabledSubsystem_Init(B_EnabledSubsystem_PI_Laengsr_T
+  *localB, P_EnabledSubsystem_PI_Laengsr_T *localP);
+extern void PI_Laengsregle_EnabledSubsystem(boolean_T rtu_Enable, const
+  SL_Bus_PI_Laengsregler_std_msgs_Float32 *rtu_In1,
+  B_EnabledSubsystem_PI_Laengsr_T *localB);
+
+#endif                               /* RTW_HEADER_PI_Laengsregler_private_h_ */
