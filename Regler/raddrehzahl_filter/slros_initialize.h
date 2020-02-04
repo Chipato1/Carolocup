4d1c9e40e542832875c71199e3104b0e1e82f479
Ben-Micha <51561475+Ben-Micha@users.noreply.github.com>
2020-02-04 20:04:13 +0100

    Upload ROS files 1

---
+++ Regler/raddrehzahl_filter/slros_initialize.h
@@ -0,0 +1,27 @@
+#ifndef _SLROS_INITIALIZE_H_
+#define _SLROS_INITIALIZE_H_
+
+#include "slros_busmsg_conversion.h"
+#include "slros_generic.h"
+
+extern ros::NodeHandle * SLROSNodePtr;
+extern const std::string SLROSNodeName;
+
+// For Block raddrehzahl_filter/Subscribe
+extern SimulinkSubscriber<std_msgs::Bool, SL_Bus_raddrehzahl_filter_std_msgs_Bool> Sub_raddrehzahl_filter_19;
+
+// For Block raddrehzahl_filter/Subscribe1
+extern SimulinkSubscriber<std_msgs::Float32, SL_Bus_raddrehzahl_filter_std_msgs_Float32> Sub_raddrehzahl_filter_3;
+
+// For Block raddrehzahl_filter/Subscribe2
+extern SimulinkSubscriber<std_msgs::Float32, SL_Bus_raddrehzahl_filter_std_msgs_Float32> Sub_raddrehzahl_filter_22;
+
+// For Block raddrehzahl_filter/Publish
+extern SimulinkPublisher<std_msgs::Float32, SL_Bus_raddrehzahl_filter_std_msgs_Float32> Pub_raddrehzahl_filter_7;
+
+// For Block raddrehzahl_filter/Publish1
+extern SimulinkPublisher<std_msgs::Float32, SL_Bus_raddrehzahl_filter_std_msgs_Float32> Pub_raddrehzahl_filter_12;
+
+void slros_node_init(int argc, char** argv);
+
+#endif
