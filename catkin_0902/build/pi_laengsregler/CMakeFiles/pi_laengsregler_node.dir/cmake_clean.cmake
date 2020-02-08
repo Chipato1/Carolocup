8cb370eaafab01d8e202d5b9885bc36a654b9898
robertkunzelmann <robert.kunzelmann@web.de>
2020-02-09 00:41:50 +0100

    regler parameter rückgängig gemacht, 20hz mit reset von i anteil . catkin hinzugefügt

---
+++ catkin_0902/build/pi_laengsregler/CMakeFiles/pi_laengsregler_node.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pi_laengsregler_node.dir/ert_main.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/PI_Laengsregler.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/PI_Laengsregler_data.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/rtGetInf.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/rtGetNaN.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/rt_nonfinite.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/linuxinitialize.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/slros_busmsg_conversion.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/slros_initialize.cpp.o"
+  "CMakeFiles/pi_laengsregler_node.dir/slros_generic_param.cpp.o"
+  "/home/robert/catkin_ws/devel/lib/pi_laengsregler/pi_laengsregler_node.pdb"
+  "/home/robert/catkin_ws/devel/lib/pi_laengsregler/pi_laengsregler_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pi_laengsregler_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_0902/build/stanley_querregler/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robert/catkin_ws/src/stanley_querregler
+# Build directory: /home/robert/catkin_ws/build/stanley_querregler
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
