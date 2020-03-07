cd46bcade0020d67510c5f3fe317f70a571378c8
robertkunzelmann <robert.kunzelmann@web.de>
2020-03-07 15:37:10 +0100

    an der motorkennlinie rumgespielt

--- Regler/old_shit/Material_fuer_PI/Motorkennlinie/Untitled.m
+++ Regler/old_shit/Material_fuer_PI/Motorkennlinie/Untitled.m
@@ -3,10 +3,15 @@ test1.Time = 1:size(DATA011.Time,1)
 test1.Data = DATA011.AccX
 sim = 130%size(DATA011.Time,1)
 sim2 = round(DATA011.Time(end))
+start = 60
+stop = 100
 backup = DATA011.Time;
-[num, denum] = butter(4,0.1);
-% sim Kalman_gefiltert.slx
-% simout.Time(3:end) = DATA008.Time(1:end)
+[num, denum] = butter(2,0.1);
+[num_cheby, denom_cheby] = cheby1(2,0.1,0.2);
+[num_bessel, denom_bessel] = besself(2,0.5);
+sim Kalman_gefiltert.slx
+xlswrite('test.xls', [(60:100)', out.simout1, out.simout2])
+% simout.Time(3:end) = DATA008.Time(1:end
 % figure(1)
 % plot(simout.Time, simout.Data(:,2))
 % figure(2)
