12c8e821d6c2917895c360ee9bbe0864a29739c2
Ben-Micha <Benne4.bp@gmail.com>
2020-05-04 22:16:15 +0200

    added parking and modified model_all file

---
+++ Regler/ParkingController/parallel_parking/Initialize_controller_parameter.m
@@ -0,0 +1,3 @@
+parking_speed = 0.1;        % speed for the parking maneuver
+delta_max = 20;             % maximum steering angle in degree
+TOF_distance_limit = 20;    % limit for TOF distance when a collision is nearly predicted 
